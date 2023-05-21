2552fa446fbaf520594b9370c6e44881f2731103
Devin Markell <dmarkell@fb.com>
2023-05-21 07:19:01 -0700

    many updates

--- tsv_to_lookfilter_dataset.sh
+++ tsv_to_lookfilter_dataset.sh
@@ -6,5 +6,5 @@ tmp_fnm="/tmp/tmp1.tsv"
 
 cat "$fnm" | head -1 > "$tmp_fnm"
 cat "$fnm" | tail -n+2  >> "$tmp_fnm"
-cat "$tmp_fnm" | python "$ANALYTICS_DIR""tsv_to_json.py" |
-python json_to_results_json.py "$prefix"
+cat "$tmp_fnm" | python3 "$ANALYTICS_DIR""tsv_to_json.py" |
+python3 json_to_results_json.py "$prefix"
